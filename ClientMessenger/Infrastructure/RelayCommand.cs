3de59a7ea2a86fe82b678ee70149f4ad2b9641c3
krotov dmytro <dk874190@gmail.com>
2023-10-28 08:46:49 +0300

    Initial commit

---
+++ ClientMessenger/Infrastructure/RelayCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFEvents.Infrastructure
+{
+    internal class RelayCommand : ICommand
+    {
+        Predicate<object> canExecute;
+        Action<object> execute;
+        public RelayCommand( Action<object> execute, Predicate<object> canExecute=null)
+        {
+            this.canExecute = canExecute;
+            this.execute = execute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            execute(parameter);
+        }
+    }
+}
